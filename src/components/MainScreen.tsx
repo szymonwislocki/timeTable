e38ca08b9167c41ec44ccb58471429097108889e
Szymon Wisłocki <111438136+szymonwislocki@users.noreply.github.com>
2023-01-21 17:15:05 +0100

    Added welcome message for new users

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,18 +1,27 @@
-import { Card, Grid } from "@mui/material";
+import { Alert, AlertTitle, Card, Grid } from "@mui/material";
 import { FC } from "react";
 import Schedules from "./Schedules";
 import Summary from "./Summary";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const MainScreen: FC = () => {
   return (
-    <Grid sx={{ mt: 1 }} container spacing={2}>
-      <Grid item xs={12} md={6}>
-        <Schedules />
+    <>
+      <Grid sx={{ mt: 1 }} container spacing={2}>
+        <Grid sx={{ width: "100%" }} item>
+          <Alert severity="info">
+            <AlertTitle>Hej! Wygląda na to, że jesteś tu pierwszy raz.</AlertTitle>
+            Naciśnij w prawym, górnym rogu na symbol <SettingsIcon sx={{ fontSize: "15px" }} /> i skonfiguruj działanie aplikacji.
+          </Alert>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Schedules />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Summary />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <Summary />
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
