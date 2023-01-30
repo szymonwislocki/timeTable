a9dfe122cb8f3b4a5d11771812f088fc6758b4c5
Szymon Wisłocki <111438136+szymonwislocki@users.noreply.github.com>
2023-01-30 20:30:15 +0100

    setDoc leaned

--- src/components/AddDialog.tsx
+++ src/components/AddDialog.tsx
@@ -20,7 +20,8 @@ const AddDialog: FC<Props> = ({ showAddNew, setShowAddNew }) => {
 
   const newShift = async (): Promise<void> => {
     setUserShifts([...userShifts, shift]);
-    await setDoc(docReference, { ...shift, email: user, id: docReference.id, time: Math.round(Math.abs(+new Date(0, 0, 0, ...shift.begin.split(":").map(Number), 0) - +new Date(0, 0, 0, ...shift.end.split(":").map(Number), 0)) / 3600000) });
+    const time = Math.round(Math.abs(+new Date(0, 0, 0, ...shift.begin.split(":").map(Number), 0) - +new Date(0, 0, 0, ...shift.end.split(":").map(Number), 0)) / 3600000);
+    await setDoc(docReference, { ...shift, email: user, id: docReference.id, time: time });
     setShowAddNew(false);
   };
 
